ae85a2495d1e82f2d1f6226f7a22cfa091e830e0
Chibuzo Idia <89045426+Domicaren1@users.noreply.github.com>
2022-10-22 08:42:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// prompt user for user name
+const username = prompt("Enter username")
+//Welcome user using username
+console.log(`Hello ${username}`);
+
+let point = 0
+let levels = 1
+let maxRange = 10
+ for (let i = 2; i<10; i++){
+   let random = Math.floor(Math.random()*i) +1;
+   
+   let myGuess = parseInt(prompt(`Guess a number between 1 and ${i}`));
+     
+if (myGuess !== random){
+  myGuess = parseInt(prompt('Try again'));
+  }if(myGuess = random){
+  myGuess = ('correct');
+}
+ }
